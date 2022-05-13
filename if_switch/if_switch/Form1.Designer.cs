486fa8fc8a66e052d59a33a2854da87ff54e68ac
ero <camillycamargos@gmail.com>
2022-05-13 10:28:34 -0300

    Inicializando...

---
+++ if_switch/if_switch/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace if_switch
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnIf = new System.Windows.Forms.Button();
+            this.btnSwitch = new System.Windows.Forms.Button();
+            this.txt_num1 = new System.Windows.Forms.TextBox();
+            this.txt_num2 = new System.Windows.Forms.TextBox();
+            this.txt_num3 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.ErrorImage = null;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(-7, -1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(426, 281);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnIf
+            // 
+            this.btnIf.AutoSize = true;
+            this.btnIf.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnIf.Location = new System.Drawing.Point(57, 192);
+            this.btnIf.Name = "btnIf";
+            this.btnIf.Size = new System.Drawing.Size(93, 39);
+            this.btnIf.TabIndex = 2;
+            this.btnIf.Text = "IF";
+            this.btnIf.UseVisualStyleBackColor = true;
+            this.btnIf.Click += new System.EventHandler(this.btnIf_Click);
+            // 
+            // btnSwitch
+            // 
+            this.btnSwitch.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSwitch.Location = new System.Drawing.Point(271, 192);
+            this.btnSwitch.Name = "btnSwitch";
+            this.btnSwitch.Size = new System.Drawing.Size(93, 39);
+            this.btnSwitch.TabIndex = 4;
+            this.btnSwitch.Text = "Switch";
+            this.btnSwitch.UseVisualStyleBackColor = true;
+            this.btnSwitch.Click += new System.EventHandler(this.btnSwitch_Click);
+            // 
+            // txt_num1
+            // 
+            this.txt_num1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_num1.Location = new System.Drawing.Point(95, 52);
+            this.txt_num1.Name = "txt_num1";
+            this.txt_num1.Size = new System.Drawing.Size(218, 30);
+            this.txt_num1.TabIndex = 5;
+            // 
+            // txt_num2
+            // 
+            this.txt_num2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_num2.Location = new System.Drawing.Point(95, 99);
+            this.txt_num2.Name = "txt_num2";
+            this.txt_num2.Size = new System.Drawing.Size(218, 30);
+            this.txt_num2.TabIndex = 7;
+            this.txt_num2.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txt_num3
+            // 
+            this.txt_num3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_num3.Location = new System.Drawing.Point(95, 145);
+            this.txt_num3.Name = "txt_num3";
+            this.txt_num3.Size = new System.Drawing.Size(218, 30);
+            this.txt_num3.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(418, 278);
+            this.Controls.Add(this.txt_num3);
+            this.Controls.Add(this.txt_num2);
+            this.Controls.Add(this.txt_num1);
+            this.Controls.Add(this.btnSwitch);
+            this.Controls.Add(this.btnIf);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnIf;
+        private System.Windows.Forms.Button btnSwitch;
+        private System.Windows.Forms.TextBox txt_num1;
+        private System.Windows.Forms.TextBox txt_num2;
+        private System.Windows.Forms.TextBox txt_num3;
+    }
+}
+
---
+++ if_switch/if_switch/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace if_switch
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSwitch_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnIf_Click(object sender, EventArgs e)
+        {
+            int num1 = int.Parse(txt_num1.Text), num2 = int.Parse(txt_num2.Text), num3 = int.Parse(txt_num3.Text);
+
+            if(num1 > num3)
+            {
+                MessageBox.Show("Verde");
+            }
+            if(num2 == num1)
+            {
+                MessageBox.Show("Amarelo");
+            }
+            if(num3 > 5)
+            {
+                MessageBox.Show("Azul");
+            }
+            if(num2 != 0)
+            {
+                MessageBox.Show("Branco");
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
